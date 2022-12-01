32e43a3078bc951d333f04d47efa353773f01a6e
ThethingOne <Jaydersmk@gmail.com>
2022-12-01 12:04:50 -0600

    leccion 6

---
+++ FundamentosCSHARP3/Program.cs
@@ -0,0 +1,46 @@
+﻿using FundamentosCSHARP.Models;
+using FundamentosCSHARP3.Models;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace FundamentosCSHARP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CervezaBD cervezaBD = new CervezaBD();
+
+
+
+            // insertamos nueva cerveza
+            /*{
+                Cerveza cerveza = new Cerveza(15, "Pale_ale");
+                cerveza.Marca = "Minerva";
+                cerveza.Alcohol = 6;
+                cervezaBD.Add(cerveza);
+            } */
+            {
+              
+                cervezaBD.Delete(5);
+            }
+
+
+
+            // obtener todas las cervezas
+            var cervezas = cervezaBD.Get();
+
+
+            foreach (var cerveza in cervezas)
+            {
+                Console.WriteLine(cerveza.Nombre);
+
+            }
+        }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Curso c# nobb/Master. 
+# leccion 6. 
+#Objeto, persona/herramientas,bla, bla.
+#Interfaces.
+#Base de datos/lista/DELETE/EDIT/ADD.
+
+# Desarrollado en C#
+
+ # Créditos
+  [J. Hernandez](https://github.com/Jorge-E-HH) autor original del contenido.
